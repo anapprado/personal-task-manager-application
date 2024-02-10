38b0fbc3552298fc4f2562ea77dab528ec732d15
Ana Prado <153234400+anapprado@users.noreply.github.com>
2024-02-09 20:11:30 -0600

    fix jason bug

--- app/tasks/[id]/DeleteTaskButton.tsx
+++ app/tasks/[id]/DeleteTaskButton.tsx
@@ -1,9 +1,25 @@
-import { Button } from '@radix-ui/themes'
+'use client';
+
+import { Spinner } from '@/app/components';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteTaskButton = ({ taskId } : { taskId: number}) => {
   return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
     <Button color = "orange">Delete Task</Button>
-  )
-}
+  </AlertDialog.Trigger>
+  <AlertDialog.Content>
+    <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+    <AlertDialog.Description>
+      Are you sure you want to delete? This action cannot be undone.
+    </AlertDialog.Description>
+  </AlertDialog.Content>
+  </AlertDialog.Root>
+  );
+};
 
 export default DeleteTaskButton
